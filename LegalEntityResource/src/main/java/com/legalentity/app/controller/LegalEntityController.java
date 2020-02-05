f4c7bf2fa456717d611d53ee87b30a3dd027a7f9
aruthoma <arunthomasbe@gmail.com>
2020-02-05 19:26:04 +0530

    Parameter Names Changed - Added More Comments

--- LegalEntityResource/src/main/java/com/legalentity/app/controller/LegalEntityController.java
+++ LegalEntityResource/src/main/java/com/legalentity/app/controller/LegalEntityController.java
@@ -1,5 +1,11 @@
 package com.legalentity.app.controller;
 
+//*******************************************************************
+//LegalEntityController.class
+// This Controller handles the incoming HTTP requests and call the service methods to fetch the information
+//  and returns the value accordingly
+//*******************************************************************
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,47 +25,53 @@ public class LegalEntityController {
 	@Autowired
 	private LegalEntityService legalEntityService;
 
-	/*http://localhost:8081/createlegalentities/  URL will create three entity objects */
+	/*http://localhost:8081/createlegalentities/  URL 
+	 * will create three entity objects */
 	@RequestMapping("/createlegalentities")
 	public List<LegalEntityBean> createLegalEntities() {
 		return legalEntityService.createLegalEntity();
 	}
 
-	/*http://localhost:8081/getAlllegalentities/  URL will show all the entity objects in the bean object*/
+	/*http://localhost:8081/getAlllegalentities/  URL 
+	 * will show all the entity objects in the bean object*/
 	@RequestMapping("/getAlllegalentities")
 	public List<LegalEntityBean> getAllLegalEntities() {
 		return legalEntityService.getAllLegalEntity();
 	}
 
-	/*http://localhost:8081/getlegalentity/{entityname}  URL will find a specific entity we are searching for .
-	we can pass the `Entity Name` as the parameter,if the sis not present, this api will return null */
+	/*http://localhost:8081/getlegalentity/{entityname}  URL 
+	 * will find a specific entity we are searching for .
+	we can pass the `Entity Name` as the parameter,if the entityName is not present, this api will return null */
 	@RequestMapping(method=RequestMethod.GET,value="/getlegalentity/{entityName}")
-	public LegalEntityBean getLegalEntityByName(@PathVariable String entityName) {
+	public List<LegalEntityBean> getLegalEntityByName(@PathVariable String entityName) {
 		return legalEntityService.getLegalEntityByName(entityName);
 	}
 
-	/*http://localhost:8081/addlegalentity URL will add a new object in the bean object and return the bean object.
-	So that we can verify if the value is added or not. This is POST request - We need some rest client to Test this */
+	/*http://localhost:8081/addlegalentity URL 
+	 * will add a new object in the bean object and return the bean object.
+	So that we can verify if the value is added or not. This is a POST request - We need some rest client to Test this */
 	@RequestMapping(method=RequestMethod.POST,value="/addlegalentity")
 	public List<LegalEntityBean> addLegalEntityByName(@RequestBody LegalEntityBean legalEntity) {
 		legalEntityService.addLegalEntityByName(legalEntity);
 		return legalEntityService.getAllLegalEntity();
 	}
 
-	/*http://localhost:8081//updatelegalentity/{entityname} URL will update in the bean object and return the bean object.
-	So that we can verify if the value is updated or not. This is PUT request - We need some rest client to Test this.
-	 We should pass the `entityname` which needs to be updated and in the request body, we should pass the JSON value , which will replace the existing value for the `Entity Name` */
-	@RequestMapping(method=RequestMethod.PUT,value="/updatelegalentity/{entityname}")
-	public List<LegalEntityBean> updateLegalEntityByName(@RequestBody LegalEntityBean legalEntityBean,@PathVariable String entityname) {
-		legalEntityService.updateLegalEntityByName(legalEntityBean, entityname);;
+	/*http://localhost:8081//updatelegalentity/{entityname} URL 
+	 * will update in the bean object and return the bean object.
+	So that we can verify if the value is updated or not. This is a PUT request - We need some rest client to Test this.
+	 We should pass the `entityName` which needs to be updated and in the request body, we should pass the JSON value , which will replace the existing value for the `Entity Name` */
+	@RequestMapping(method=RequestMethod.PUT,value="/updatelegalentity/{entityName}")
+	public List<LegalEntityBean> updateLegalEntityByName(@RequestBody LegalEntityBean legalEntityBean,@PathVariable String entityName) {
+		legalEntityService.updateLegalEntityByName(legalEntityBean, entityName);
 		return legalEntityService.getAllLegalEntity();
 	}
 
-	/*http://localhost:8081//deletelegalentity/{entityname} URL will delete a specific object from the bean object and return the bean object.
-	So that we can verify if the value is deleted or not. This is DELETE request */
-	@RequestMapping(method=RequestMethod.DELETE,value="/deletelegalentity/{entityname}")
-	public List<LegalEntityBean> deleteLegalEntityByName(@PathVariable String entityname) {
-		legalEntityService.deleteLegalEntityByName( entityname);;
+	/*http://localhost:8081//deletelegalentity/{entityname} URL 
+	 * will delete a specific object from the bean object and return the bean object.
+	So that we can verify if the value is deleted or not. This is a DELETE request */
+	@RequestMapping(method=RequestMethod.DELETE,value="/deletelegalentity/{entityName}")
+	public List<LegalEntityBean> deleteLegalEntityByName(@PathVariable String entityName) {
+		legalEntityService.deleteLegalEntityByName( entityName);;
 		return legalEntityService.getAllLegalEntity();
 	}
 	
